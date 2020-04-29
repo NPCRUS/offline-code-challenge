6e86fff2c42abc40785f90cade3776a456b14672
Npcrus <npcrus@DENBGNBM0803.local>
2020-04-29 11:25:09 +0200

    usersRoute tests

--- src/test/scala/UsersRouteSpec.scala
+++ src/test/scala/UsersRouteSpec.scala
@@ -1,17 +1,41 @@
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import models.UserPost
+import models.{User, UserPost}
 import JsonSupport._
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Route
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
 class UsersRouteSpec extends AnyWordSpecLike with Matchers with ScalatestRouteTest {
   val usersRoute: Route = Router.usersRouter
+  val userPost: UserPost = UserPost("test", "test", "test@test.com")
+  val userPostRequest: HttpRequest = Post("/users", userPost)
 
   "UsersRoute post" should {
     "return id of newly created user" in {
-      Post("/users", UserPost("test", "test", "test")) ~> usersRoute ~> check {
+      userPostRequest ~> usersRoute ~> check {
         responseAs[String].shouldEqual("1")
+      }
+    }
+    "return 409 if user with such email exists" in {
+      userPostRequest ~> usersRoute
+      userPostRequest ~> usersRoute ~> check {
+        status.shouldEqual(Conflict)
+      }
+    }
+  }
+
+  "UsersRoute get" should {
+    "return empty array by default" in {
+      Get("/users") ~> usersRoute ~> check {
+        responseAs[String].shouldEqual("[]")
+      }
+    }
+    "return list of users" in {
+      userPostRequest ~> usersRoute
+      Get("/users") ~> usersRoute ~> check {
+        responseAs[List[User]].shouldEqual(List(User.fromPost(1, userPost)))
       }
     }
   }
